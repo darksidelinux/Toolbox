285bb50eb38024e9bf3a6fea919a81d9721a9dd3
darksidelinux <153812795+darksidelinux@users.noreply.github.com>
2023-12-19 15:33:50 +0300

    Toolbox Updated to V0.02PR

--- toolbox.bat
+++ toolbox.bat
@@ -1,7 +1,7 @@
 @echo off
-title DarkHub Toolbox v0.01 - Waiting for Admin Privileges...
+title DarkHub Toolbox - Waiting for Admin Privileges...
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-title DarkHub Toolbox v0.01
+title DarkHub Toolbox
 mode con: cols=70 lines=4
 cls && color 01
 echo  ====================================================================
@@ -9,16 +9,17 @@ echo                        Initializing Toolbox...
 echo  ====================================================================
 timeout /t 5 >nul
 mode con: cols=100 lines=30
+set PRVersion=v0.02
 goto mainmenu
 :mainmenu
 cls && color a
 echo  ==================================================================================================
-echo                                 Welcome to DarkHub Toolbox v0.01
+echo                                 Welcome to DarkHub Toolbox %PRVersion%
 echo   User: %username% - Machine: %computername% - Main Drive: %HOMEDRIVE%\ - Date: %DATE% - %TIME%
 echo  ==================================================================================================
 echo.
-echo  [34m[1][0m System Tools(1)
-echo.
+echo  [34m[1][0m System Tools(2)
+echo  [34m[2][0m Downloads(3)
 echo.
 echo.
 echo.
@@ -43,16 +44,154 @@ echo.
 echo.
 set /p choice=  Choose: 
 if %choice%==1 goto system
+if %choice%==2 goto chocolatey
+
+:chocolatey
+cls && color a
+echo  ==================================================================================================
+echo                                 Welcome to DarkHub Toolbox %PRVersion%
+echo   User: %username% - Machine: %computername% - Main Drive: %HOMEDRIVE%\ - Date: %DATE% - %TIME%
+echo  ==================================================================================================
+echo.
+echo  [34m[1][0m Download Chocolatey First                              [33m[B][0m Back
+echo  [34m[2][0m Web Browsers(3)
+echo  [34m[3][0m Messaging(2)
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+set /p systools=Choose:
+if %systools%==1 goto dlchoco
+if %systools%==2 goto webbrowsers
+if %systools%==3 goto messaging
+if %systools%==b goto mainmenu
+if %systools%==B goto mainmenu
+
+:dlchoco
+powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+goto chocolatey
+
+:webbrowsers
+cls && color a
+echo  ==================================================================================================
+echo                                 Welcome to DarkHub Toolbox %PRVersion%
+echo   User: %username% - Machine: %computername% - Main Drive: %HOMEDRIVE%\ - Date: %DATE% - %TIME%
+echo  ==================================================================================================
+echo.
+echo  [34m[1][0m Mozilla Firefox                                        [33m[B][0m Back
+echo  [34m[2][0m Opera
+echo  [34m[2][0m Opera GX
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+set /p cleanperform= Choose: 
+if %cleanperform%==1 goto firefox
+if %cleanperform%==2 goto opera
+if %cleanperform%==3 goto operagx
+if %cleanperform%==b goto chocolatey
+if %cleanperform%==B goto chocolatey
+
+:firefox
+choco install -y firefox
+goto webbrowsers
+
+:opera
+choco install -y opera
+goto webbrowsers
+
+:operagx
+choco install -y opera-gx
+goto webbrowsers
+
+:messaging
+cls && color a
+echo  ==================================================================================================
+echo                                 Welcome to DarkHub Toolbox %PRVersion%
+echo   User: %username% - Machine: %computername% - Main Drive: %HOMEDRIVE%\ - Date: %DATE% - %TIME%
+echo  ==================================================================================================
+echo.
+echo  [34m[1][0m Discord                                                [33m[B][0m Back
+echo  [34m[2][0m Whatsapp
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+set /p cleanperform= Choose: 
+if %cleanperform%==1 goto discord
+if %cleanperform%==2 goto whatsapp
+if %cleanperform%==b goto chocolatey
+if %cleanperform%==B goto chocolatey
+
+:discord
+choco install -y --ignore-checksums discord
+goto messaging
+
+:whatsapp
+choco install -y --ignore-checksums whatsapp
+goto messaging
 
 :system
 cls && color a
 echo  ==================================================================================================
-echo                                 Welcome to DarkHub Toolbox v0.01
+echo                                 Welcome to DarkHub Toolbox %PRVersion%
 echo   User: %username% - Machine: %computername% - Main Drive: %HOMEDRIVE%\ - Date: %DATE% - %TIME%
 echo  ==================================================================================================
 echo.
 echo  [34m[1][0m Power Management(11)                                   [33m[B][0m Back
-echo.
+echo  [34m[2][0m Cleaning and Performance(6)
 echo.
 echo.
 echo.
@@ -77,13 +216,80 @@ echo.
 echo.
 set /p systools=Choose:
 if %systools%==1 goto power
+if %systools%==2 goto cp
 if %systools%==b goto mainmenu
 if %systools%==B goto mainmenu
+
+:cp
+cls && color a
+echo  ==================================================================================================
+echo                                 Welcome to DarkHub Toolbox %PRVersion%
+echo   User: %username% - Machine: %computername% - Main Drive: %HOMEDRIVE%\ - Date: %DATE% - %TIME%
+echo  ==================================================================================================
+echo.
+echo  [34m[1][0m Clean Temp                                             [33m[B][0m Back
+echo  [34m[2][0m Clean Prefetch
+echo  [34m[3][0m Defrag(%HOMEDRIVE%)
+echo  [34m[4][0m Defrag(All Disks)
+echo  [34m[5][0m Clean Manager(%HOMEDRIVE%)
+echo  [34m[6][0m Clean Manager(All Disks)
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+set /p cleanperform= Choose: 
+if %cleanperform%==1 goto ctemp
+if %cleanperform%==2 goto cpfetch
+if %cleanperform%==3 goto cdefhome
+if %cleanperform%==4 goto cdefall
+if %cleanperform%==5 goto cclnmgrhome
+if %cleanperform%==6 goto cclnmgrauto
+if %cleanperform%==b goto system
+if %cleanperform%==B goto system
+
+:ctemp
+rd %temp% /s /q
+rd C:\Windows\Temp /s /q
+goto cp
+
+:cpfetch
+rd C:\Windows\Prefetch /s /q
+goto cp
+
+:cdefhome
+defrag %HOMEDRIVE% /H /M
+goto cp
+
+:cdefall
+defrag /C /H /M
+goto cp
+
+:cclnmgrhome
+cleanmgr.exe /d%HOMEDRIVE% /verylowdisk
+goto cp
+
+:cclnmgrauto
+cleanmgr.exe /AUTOCLEAN /verylowdisk
+goto cp
 
 :power
 cls && color a
 echo  ==================================================================================================
-echo                                 Welcome to DarkHub Toolbox v0.01
+echo                                 Welcome to DarkHub Toolbox %PRVersion%
 echo   User: %username% - Machine: %computername% - Main Drive: %HOMEDRIVE%\ - Date: %DATE% - %TIME%
 echo  ==================================================================================================
 echo.
